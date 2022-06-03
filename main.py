6f37d19d787132c33a5d4efd6e00125225f7ea78
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-06-03 20:37:16 +0300

    Exercise-2

---
+++ main.py
@@ -0,0 +1,23 @@
+class Calculator:
+
+    def add(self, a, b):
+        self._a = a
+        self._b = b
+        return self._a + self._b
+
+    def subtract(self, a, b):
+        self._a = a
+        self._b = b
+        return self._a - self._b
+
+    def multiply(self, a, b):
+        self._a = a
+        self._b = b
+        return self._a * self._b
+
+    def divide(self, a, b):
+        self._a = a
+        self._b = b
+        return self._a / self._b
+
+
